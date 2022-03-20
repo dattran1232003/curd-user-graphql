08696e96d8d1117f3f29d52a098f9c5a9bd5e780
Dat Tran <quoc-dat.tran@ant-tech.eu>
2022-03-20 17:32:13 +0700

    feat: validation

--- src/api/users/types/sign-up.input.ts
+++ src/api/users/types/sign-up.input.ts
@@ -1,17 +1,23 @@
+import { IsEmail, IsString, MinLength } from 'class-validator'
 import { Field, InputType } from 'type-graphql'
 import { USER_TYPE } from '../constants'
 
 @InputType()
 export class SignUpInput {
   @Field()
+  @IsString()
   username: string
 
   @Field(_ => USER_TYPE)
   userType: USER_TYPE
 
   @Field()
+  @IsEmail()
+  @IsString()
   email: string
 
   @Field()
+  @MinLength(6)
+  @IsString()
   password: string
 }
